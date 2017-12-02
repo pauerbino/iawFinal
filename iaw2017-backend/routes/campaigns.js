8480269f62f0ed09e2ed014e188e0ccccba9860c
Joaquin Pujol <pujoljoaquin@hotmail.com>
2017-12-02 13:10:55 -0300

    cambios

--- iaw2017-backend/routes/campaigns.js
+++ iaw2017-backend/routes/campaigns.js
@@ -46,7 +46,7 @@ router.post('/', function(req, res, next) {
             list : req.body.list,
             mails : [],
             content : req.body.content,
-            participants : req.body.participants
+            participants : req.body.participants,
             user : u[0]._id
         });
 
